513e5cb4106451e6fec14deec9878d93d27f9cff
Max Flores <maxfloreshn@gmail.com>
2021-09-13 05:32:09 -0600

    Ready for deployment

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,73 +1,98 @@
 import React, { useState, useEffect } from 'react';
 import { CssBaseline, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
-// import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import { commerce } from '../../../lib/commerce';
-import useStyles from './styles';
 import AddressForm from '../AddressForm';
 import PaymentForm from '../PaymentForm';
+import useStyles from './styles';
 
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
-  const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
+  const [activeStep, setActiveStep] = useState(0);
   const [shippingData, setShippingData] = useState({});
+  
   const classes = useStyles();
-
-  useEffect(() => {
-    const generateToken = async ()  => {
-      try {
-        const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
-
-        setCheckoutToken(token);
-      } catch(error) {
-      
-      }
-    }
-
-    generateToken();
-  }, [cart]);
+  const history = useHistory();
 
   const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
   const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
 
-  const next = (data) => {
+  useEffect(() => {
+    if (cart.id) {
+      const generateToken = async ()  => {
+        try {
+          const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
+
+          setCheckoutToken(token);
+        } catch {
+            if (activeStep !== steps.lenght) history.push('/');
+        }
+      };
+
+      generateToken();
+    }
+  }, [cart, activeStep, history]);
+
+
+  const test = (data) => {
     setShippingData(data);
 
     nextStep();
   }
 
-  const Confirmation = () => (
+  let Confirmation = () => order.customer ? (
       <>
-       Confirmation
+       <div>
+         <Typography variant="h5">Thank you for your purchase, {order.customer.name} {order.customer.lastname}!</Typography>
+         <Divider  classes={classes.divider}/>
+         <Typography variant="subtitle2">Order ref: {order.customer_reference}</Typography>
+       </div>
+       <br />
+       <Button component={Link} variant="outlined" type="button" to="/" >Back to Home</Button>
       </>
+    ) : (
+      <div classes={classes.spinner}>
+          <CircularProgress />
+      </div>
     );
-  
+    // MISSING
 
+    if (error) {
+      Confirmation = () => (
+        <>
+          <Typography variant="h5" >Error: {error}</Typography>
+          <br />
+          <Button component={Link} variant="outlined" type="button" to="/" >Back to Home</Button>
+        </>
+        );
+    }
+  
   const Form = () => (activeStep === 0
-    ? <AddressForm checkoutToken={checkoutToken} next={next}  />
-    : <PaymentForm checkoutToken={checkoutToken} checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} onCaptureCheckout={onCaptureCheckout}/>);
+    ? <AddressForm checkoutToken={checkoutToken} nextSteo={nextStep} setShippingData={setShippingData} test={test} />
+    : <PaymentForm checkoutToken={checkoutToken} nextStep={nextStep} backStep={backStep} shippingData={shippingData} onCaptureCheckout={onCaptureCheckout}/>);
 
   return (
     <>
-      <CssBaseline />
+    <CssBaseline />
       <div className={classes.toolbar} />
       <main className={classes.layout}>
         <Paper className={classes.paper}>
           <Typography variant="h4" align="center">Checkout</Typography>
           <Stepper activeStep={activeStep} className={classes.stepper}>
-            {steps.map((step) => (
-              <Step key={step}>
-                <StepLabel>{step}</StepLabel>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
               </Step>
             ))}
           </Stepper>
-          {/* {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />} */}
+          {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />}
         </Paper>
       </main>
     </>
   );
-}
+};
 
 export default Checkout;
--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -1,22 +1,20 @@
+  
 import React from 'react';
-import { TextField, Grid } from '@material-ui/core';
 import { useFormContext, Controller } from 'react-hook-form';
+import { TextField, Grid } from '@material-ui/core';
 
-const FormInput = ({ name, label }) => {
-    const { control } = useFormContext();
+function FormInput({ name, label, required }) {
+  const { control } = useFormContext();
 
-    return (
-        <Grid item xs={12} sm={6}>
-            <Controller 
-                as={TextField}
-                control={control}
-                fullWith
-                name={name}
-                label={label}
-                required
-            />            
-        </Grid>
-    );
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller
+          control={control}
+	        name={name}
+	        render={({ field }) => <TextField fullWidth label={label} required />}
+      />
+    </Grid>
+  );
 }
 
 export default FormInput;
